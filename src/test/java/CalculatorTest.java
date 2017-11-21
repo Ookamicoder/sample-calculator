fefa8963b24d9c0876f0393f5ea7b3e65014587a
Erik Edlund <erik_edlund@hotmail.se>
2017-11-21 11:21:59 +0100

    Added testcases for negative numbers to Multiply

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -280,5 +280,88 @@ public class CalculatorTest {
 			
 		}
 	}
+	
+	@Test
+	public void testMultiplyLargePositiveNumbers() {
+		double firstNumber = 0;
+		double secondNumber = 0;
+		double result = 0;
+		
+		for(int i = 0;i<200;i++) {
+			firstNumber = Double.valueOf(df.format(random.nextDouble()*1000));
+			secondNumber = Double.valueOf(df.format(random.nextDouble()*1000));
+			result = firstNumber * secondNumber;
+			
+			LOG.info("Testing the method with: "+ firstNumber +" and " + secondNumber);
+			assertEquals(Math.round(calculator.multiply(firstNumber, secondNumber)), Math.round(result),1);
+			
+		}
+	}
+	
+	@Test
+	public void testMultiplyZeros() {
+		double firstNumber = 0;
+		double secondNumber = 0;
+		double result = 0;
+		
+		for(int i = 0;i<200;i++) {
+			result = firstNumber * secondNumber;
+			
+			LOG.info("Testing the method with: "+ firstNumber +" and " + secondNumber);
+			assertEquals(Math.round(calculator.multiply(firstNumber, secondNumber)), Math.round(result),1);
+			
+		}
+	}
+	
+	@Test
+	public void testMultiplySmallNegativeNumbers() {
+		double firstNumber = 0;
+		double secondNumber = 0;
+		double result = 0;
+		
+		for(int i = 0;i<200;i++) {
+			firstNumber = Double.valueOf(df.format(random.nextDouble()*-10));
+			secondNumber = Double.valueOf(df.format(random.nextDouble()*-10));
+			result = firstNumber * secondNumber;
+			
+			LOG.info("Testing the method with: "+ firstNumber +" and " + secondNumber);
+			assertEquals(Math.round(calculator.multiply(firstNumber, secondNumber)), Math.round(result),1);
+			
+		}
+	}
+	
+	@Test
+	public void testMultiplyMediumNegativeNumbers() {
+		double firstNumber = 0;
+		double secondNumber = 0;
+		double result = 0;
+		
+		for(int i = 0;i<200;i++) {
+			firstNumber = Double.valueOf(df.format(random.nextDouble()*-100));
+			secondNumber = Double.valueOf(df.format(random.nextDouble()*-100));
+			result = firstNumber * secondNumber;
+			
+			LOG.info("Testing the method with: "+ firstNumber +" and " + secondNumber);
+			assertEquals(Math.round(calculator.multiply(firstNumber, secondNumber)), Math.round(result),1);
+			
+		}
+	}
+	
+	@Test
+	public void testMultiplyLargeNegativeNumbers() {
+		double firstNumber = 0;
+		double secondNumber = 0;
+		double result = 0;
+		
+		for(int i = 0;i<200;i++) {
+			firstNumber = Double.valueOf(df.format(random.nextDouble()*-1000));
+			secondNumber = Double.valueOf(df.format(random.nextDouble()*-1000));
+			result = firstNumber * secondNumber;
+			
+			LOG.info("Testing the method with: "+ firstNumber +" and " + secondNumber);
+			assertEquals(Math.round(calculator.multiply(firstNumber, secondNumber)), Math.round(result),1);
+			
+		}
+	}
 
 }
